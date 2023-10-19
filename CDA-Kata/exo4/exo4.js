64ae7e73b7a8d7c3c7abcd0c1eeac275ab6359ee
HardstyIe <hardstyle.dd@gmail.com>
2023-10-19 14:31:55 +0200

    fin du test CDA

---
+++ CDA-Kata/exo4/exo4.js
@@ -0,0 +1,25 @@
+// # Minimum
+
+// Write a function min that takes two arguments and returns their minimum.
+
+
+// ```
+// console.log(min(0, 10));
+// // → 0
+// console.log(min(0, -10));
+// // → -10
+
+// ```
+
+function min(a, b) {
+    if (a < b) {
+        return a
+    } else {
+        return b
+    }
+}
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
